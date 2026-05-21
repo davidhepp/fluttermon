3174ee4b33238987191a3d45c7903164eec09106
David Heppenheimer <david@heppify.de>
2026-05-21 20:50:38 +0200

    feat: persist theme mode selection

--- lib/models/app_settings.dart
+++ lib/models/app_settings.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppSettings {
-  const AppSettings({required this.themeMode, required this.languageCode});
+  const AppSettings({required this.themeMode});
 
   final ThemeMode themeMode;
-  final String languageCode;
 
-  AppSettings copyWith({ThemeMode? themeMode, String? languageCode}) {
-    return AppSettings(
-      themeMode: themeMode ?? this.themeMode,
-      languageCode: languageCode ?? this.languageCode,
-    );
+  AppSettings copyWith({ThemeMode? themeMode}) {
+    return AppSettings(themeMode: themeMode ?? this.themeMode);
   }
 }
