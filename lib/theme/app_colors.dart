2ba3b080c4e5818b6b5399899801bbdda952ec88
David Heppenheimer <david@heppify.de>
2026-05-21 20:45:39 +0200

    style: centralize theme colors

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+
+  static const yellow = Color(0xFFFFCB05);
+  static const navy = Color(0xFF0B1F3A);
+  static const darkBlue = Color(0xFF0D47A1);
+
+  static const lightBackground = Colors.white;
+  static const lightSurface = Colors.white;
+  static const lightText = navy;
+
+  static const darkBackground = Color(0xFF101923);
+  static const darkSurface = Color(0xFF162231);
+  static const darkText = Colors.white;
+
+  static const transparent = Colors.transparent;
+  static const white = Colors.white;
+  static const headerButtonBackground = Color(0x59000000);
+  static const lightHeaderOverlay = Color(0x33000000);
+  static const darkHeaderOverlay = Color(0x66000000);
+}
